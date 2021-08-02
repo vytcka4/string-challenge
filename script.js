fd88e82ee04447ac2571094ca08685ed69cb426c
vytcka4 <vytautas.freelance@gmail.com>
2021-08-02 23:32:56 +0300

    commit

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
   const input = text.value;
-  const inputNormal = [];
-  for (let i = 0; i < input.length; i++) {
-    inputNormal.push(input[i].toLowerCase().trim().replace("_", ""));
+  const row = input.split("\n");
+  for (row2 of row) {
+    const indexUpper = row2.indexOf("_");
+    let divideWord = row2.split("_");
+    const joint = divideWord.join("").toLowerCase();
+    const joint2 = joint
+      .replace(joint[indexUpper], joint[indexUpper].toUpperCase())
+      .trim();
+    // console.log(divideWord);
+    // divideWord.join("");
+    console.log(joint2);
+
+    // const indexUpper = row2.indexOf("_");
+    // indexUpperCase = indexUpper.toCase();
+    // console.log(indexUpper);
+    // console.log(row2.toLowerCase().replace("_", "").trim());
   }
-  return console.log(inputNormal);
 });
